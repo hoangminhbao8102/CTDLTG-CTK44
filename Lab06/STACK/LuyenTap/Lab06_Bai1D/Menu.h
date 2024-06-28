53042028a3a9928f013dfe02deb80cdff87d9a55
hoangminhbao8102 <90952548+hoangminhbao8102@users.noreply.github.com>
2024-06-28 16:47:53 +0700

    main

---
+++ Lab06/STACK/LuyenTap/Lab06_Bai1D/Menu.h
@@ -0,0 +1,114 @@
+﻿void XuatMenu();
+int ChonMenu(int SoMenu);
+void XuLyMenu(int menu, STACK& s);
+
+void XuatMenu()
+{
+	cout << "\n================ He thong chuc nang ===============";
+	cout << "\n0. Thoat khoi chuong trinh";
+	cout << "\n1. Nhap du lieu";
+	cout << "\n2. Xuat du lieu";
+	cout << "\n3. Them mot node o dau stack";
+	cout << "\n4. Huy mot node o dau stack";
+	cout << "\n5. Dem so lan xuat hien x trong stack";
+	cout << "\n6. Xoa toan bo nhung node co du lieu x trong stack";
+	cout << "\n7. Xem du lieu";
+	cout << "\n===================================================";
+}
+
+int ChonMenu(int SoMenu)
+{
+	int STT;
+	for (;;)
+	{
+		system("CLS");
+		XuatMenu();
+		cout << "\nNhap mot so ( 0 <= so <= " << SoMenu << " ) de chon menu, stt = ";
+		cin >> STT;
+		if (0 <= STT && STT <= SoMenu)
+		{
+			break;
+		}
+	}
+	return STT;
+}
+
+void XuLyMenu(int menu, STACK& s)
+{
+	char filename[MAX];
+	int kq;
+	Data x;
+	switch (menu)
+	{
+	case 0:
+		system("CLS");
+		cout << "\n0. Thoat khoi chuong trinh\n";
+		break;
+	case 1:
+		system("CLS");
+		cout << "\n1. Nhap du lieu\n";
+		do
+		{
+			cout << "\nNhap ten tap tin, filename = ";
+			_flushall();
+			cin >> filename;
+			kq = File_Stack(filename, s);
+			if (!kq)
+				cout << "\nLoi mo file ! nhap lai\n";
+		} while (!kq);
+		cout << endl;
+		break;
+	case 2:
+		system("CLS");
+		cout << "\n2. Xuat du lieu\n";
+		Output_Stack(s);
+		break;
+	case 3:
+		system("CLS");
+		cout << "\n3. Them mot node o dau stack\n";
+		cout << "\nNhap du lieu cho node moi: ";
+		cin >> x;
+		Push(s, x);
+		break;
+	case 4:
+		system("CLS");
+		cout << "\n4. Huy mot node o dau stack\n";
+		if (!IsEmpty(s)) 
+		{
+			x = Pop(s);
+			cout << "\nDu lieu node bi xoa: " << x << endl;
+		}
+		else 
+		{
+			cout << "\nStack rong!\n";
+		}
+		break;
+	case 5:
+		system("CLS");
+		cout << "\n5. Dem so lan xuat hien x trong stack\n";
+		cout << "\nNhap gia tri x: ";
+		cin >> x;
+		cout << "\nSo lan xuat hien cua " << x << " trong stack: " << Count(s, x) << endl;
+		break;
+	case 6:
+		system("CLS");
+		cout << "\n6. Xoa toan bo nhung node co du lieu x trong stack\n";
+		cout << "\nNhap gia tri x can xoa: ";
+		cin >> x;
+		RemoveAll(s, x);
+		break;
+	case 7:
+		system("CLS");
+		cout << "\n7. Xem du lieu\n";
+		if (!IsEmpty(s)) 
+		{
+			x = Top(s);
+			cout << "\nDu lieu o dau stack: " << x << endl;
+		}
+		else 
+		{
+			cout << "\nStack rong!\n";
+		}
+		break;
+	}
+}
---
+++ Lab06/STACK/LuyenTap/Lab06_Bai1D/ThuVien.h
@@ -0,0 +1,197 @@
+﻿#define MAX 100
+
+typedef int Data;
+
+struct TagNode
+{
+	Data info;
+	TagNode* pNext;
+};
+
+typedef TagNode NODE;
+
+struct STACK
+{
+	NODE* pHead;
+	NODE* pTail;
+};
+
+NODE* GetNode(Data x);
+void CreatStack(STACK& s);
+void InsertTail(STACK& s, Data x);
+int File_Stack(const char* f, STACK& s);
+int IsEmpty(STACK s);
+void Output_Stack(STACK s);
+
+void Push(STACK& s, Data x);
+Data Pop(STACK& s);
+int Count(STACK& s, Data x);
+void RemoveAll(STACK& s, Data x);
+Data Top(STACK s);
+
+NODE* GetNode(Data x)
+{
+	NODE* p;
+	p = new NODE;
+	if (p != NULL)
+	{
+		p->info = x;
+		p->pNext = NULL;
+	}
+	return p;
+}
+
+void CreatStack(STACK& s)
+{
+	s.pHead = s.pTail = NULL;
+}
+
+void InsertTail(STACK& s, Data x)
+{
+	NODE* new_ele = GetNode(x);
+	if (new_ele == NULL)
+	{
+		cout << "\nLoi cap phat bo nho!";
+		return;
+	}
+	if (s.pHead == NULL)
+	{
+		s.pHead = new_ele;
+		s.pTail = s.pHead;
+	}
+	else
+	{
+		s.pTail->pNext = new_ele;
+		s.pTail = new_ele;
+	}
+}
+
+int File_Stack(const char* f, STACK& s)
+{
+	ifstream in(f);
+	if (!in)
+		return 0;
+	CreatStack(s);
+	Data x;
+	while (!in.eof())
+	{
+		in >> x;
+		InsertTail(s, x);
+	}
+	in.close();
+	return 1;
+}
+
+int IsEmpty(STACK s)
+{
+	if (s.pHead == NULL)
+		return 1;
+	return 0;
+}
+
+void Output_Stack(STACK s)
+{
+	NODE* p;
+	if (s.pHead == NULL)
+	{
+		cout << "\nStack rong!\n";
+		return;
+	}
+	p = s.pHead;
+	while (p != NULL)
+	{
+		cout << p->info << '\t';
+		p = p->pNext;
+	}
+}
+
+void Push(STACK& s, Data x)
+{
+	NODE* new_ele = GetNode(x);
+	if (new_ele == NULL)
+	{
+		cout << "\nsoi cap phat bo nho! khong thuc hien duoc thao tac nay";
+		return;
+	}
+	if (s.pHead == NULL)
+	{
+		s.pHead = new_ele; s.pTail = s.pHead;
+	}
+	else
+	{
+		new_ele->pNext = s.pHead;
+		s.pHead = new_ele;
+	}
+}
+
+Data Pop(STACK& s)
+{
+	Data x;
+	NODE* p;
+	if (s.pHead == NULL)
+	{
+		return NULL;
+	}
+	x = s.pHead->info;
+	p = s.pHead;
+	s.pHead = s.pHead->pNext;
+	delete p;
+	if (s.pHead == NULL)
+		s.pTail = NULL;
+	return x;
+}
+
+int Count(STACK& s, Data x) 
+{
+	int count = 0;
+	NODE* pNode = s.pHead;
+	while (pNode != nullptr) 
+	{
+		if (pNode->info == x) 
+		{
+			count++;
+		}
+		pNode = pNode->pNext;
+	}
+	return count;
+}
+
+void RemoveAll(STACK& s, Data x) 
+{
+	NODE* current = s.pHead;
+	NODE* prev = nullptr;
+
+	while (current != nullptr) 
+	{
+		if (current->info == x) 
+		{
+			if (prev == nullptr) 
+			{ // Node to remove is the head
+				s.pHead = current->pNext;
+				delete current;
+				current = s.pHead;
+			}
+			else 
+			{
+				prev->pNext = current->pNext;
+				delete current;
+				current = prev->pNext;
+			}
+		}
+		else 
+		{
+			prev = current;
+			current = current->pNext;
+		}
+	}
+}
+
+Data Top(STACK s)
+{
+	if (IsEmpty(s))
+	{
+		cout << "Stack rong!" << endl;
+		return -1; // hoặc một giá trị nào đó phù hợp với ngữ cảnh của bạn
+	}
+	return s.pHead->info;
+}
